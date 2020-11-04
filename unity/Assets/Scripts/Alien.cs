35759043f7b3b6a81535116c8fbba833ec65ffa0
roxyrozh <53717054+roxyrozh@users.noreply.github.com>
2020-11-05 00:11:22 +1100

    scorekeeper script

---
+++ unity/Assets/Scripts/Alien.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Alien : MonoBehaviour
+{
+
+    public float alienTimer; //5
+
+    void Update()
+    {
+
+        alienTimer -= Time.deltaTime;
+
+        if (alienTimer <= 0) //after some time, the alien despawns
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Planet")
+        {
+            Counter.counter += 10; //adds points to the point counter script
+
+            Destroy(gameObject); 
+        }
+    }
+
+
+
+}
