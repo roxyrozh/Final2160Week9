dc7c6f7fb00af497590ffb8bca6f41ff8d5cfb35
roxyrozh <53717054+roxyrozh@users.noreply.github.com>
2020-11-05 00:24:43 +1100

    revised script after B's code reviewing

---
+++ unity/Assets/Scripts/Counter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+
+    public static int counter = 0;
+    Text aliens;
+
+    void Start()
+    {
+        aliens = GetComponent<Text>(); //getting the component text is attatched to 
+    }
+
+    void Update()
+    {
+        aliens.text = "Aliens: " + counter; //sets the point counter text
+    }
+}
